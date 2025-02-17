bc8635ca461e1aa41edf842aab55ec8771617fbb
Darlington02 <darlingtonnnam@gmail.com>
2025-02-17 20:19:30 +0100

    chore: update irys example

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,46 @@
-import {Starknet} from "@irys/upload-starknet-node"
-
 import { Uploader } from "@irys/upload";
-import Query from "@irys/query";
-// Example usage
-const userAddress = "";
-const privateKey = "";
+import { Starknet } from "@irys/upload-starknet-node";
+import 'dotenv/config'
 
-const createUploaderInstance = async (address: string, privateKey: string) => {
+const userAddress = "0x02d904Aedff382C0D68F22444B525146ec5eA2926e271fC411845e8D9E751DE1";
+const privateKey = process.env.PRIVATE_KEY;
+const rpcUrl = "https://starknet-sepolia.public.blastapi.io";
+
+const getIrysUploader = async() => {
+  const irysUploader = await Uploader(Starknet(userAddress)).withWallet(privateKey).devnet().withRpc(rpcUrl);
+  return irysUploader;
+}
+
+const uploadData = async() => {
+  const irysUploader = await getIrysUploader();
+  const dataToUpload = "Hello world";
+
   try {
-    const starknetInstance = Starknet(address);
+    // upload text
+    const receipt = await irysUploader.upload(dataToUpload);
+    console.log(`Data uploaded ===> https://gateway.irys.xyz/${receipt.id}`);
 
-    // Create Uploader instance
-    const client = await Uploader(starknetInstance)
-      .withWallet(privateKey)
-      .devnet()
-      .withRpc("https://starknet-sepolia.public.blastapi.io");
-// const post_data = await client.upload("Hello world")
-// const upload_file = client.uploadFile()
-const output = client.query({url:"https://devnet.irys.xyz/graphql"}).search("irys:transactions").ids(["49xGw7akCBCjmkpjoemdgKTXNyLEALPKFHT2KwF3YcYq", "CtfNife5BJEkLadkESRsfn6cVYGtF4EJn8tzm9HBBLyb", "Gmue9Dy1KNDEfDHAsRPSfK7ebEKiydQ6eFPqvGpQptnn"])
-  console.log("output:", output)
-      
-  } catch (error) {
-    console.error("Error creating uploader instance:", error);
+    // upload image
+    const image = "./images/pfp.jpg";
+    const tags = [{ name: "coloniz", value: "PFP" }];
+    const receipt2 = await irysUploader.uploadFile(image, { tags: tags });
+    console.log(`Data uploaded ===> https://gateway.irys.xyz/${receipt2.id}`);
   }
-};
+  catch(error) {
+    console.log(error);
+  }
+}
 
+const queryData = async() => {
+  const irysUploader = await getIrysUploader();
 
+  const output = irysUploader
+    .query({url:"https://devnet.irys.xyz/graphql"})
+    .search("irys:transactions")
+    .ids(["3Lt6qPMteJmmh7z184kziSwCjUDpB83tQjRuaezZu97j", "6HfJjfwHTEmnraxoeCWuRC1jC1DGc1x9i2ZdjVPMdJFw"]);
 
-createUploaderInstance(userAddress, privateKey);
+  console.log("output:", output)
+}
+
+uploadData();
+// queryData();
